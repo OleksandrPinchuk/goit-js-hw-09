a84ebf2cae49d1f9cff6581d2d84813f621465d4
OleksandrPinchuk <hohhua@gmail.com>
2023-04-25 01:00:07 +0200

    the first task is completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-// hallo
+const startBtnEl = document.querySelector('button[data-start]');
+const stopBtnEl = document.querySelector('button[data-stop]');
+
+startBtnEl.addEventListener(`click`, onStartBtnClick);
+stopBtnEl.addEventListener(`click`, onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
+let timerId = null;
+
+function onStartBtnClick() {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+};
+
+function onStopBtnClick() {
+    clearInterval(timerId);
+    startBtnEl.disabled = false;
+    stopBtnEl.disabled = true;
+};
